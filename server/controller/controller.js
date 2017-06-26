9da35f4921e744256a9377108e9ff47d2d5c06f7
davidmashe <davidmashe@gmail.com>
2017-06-25 22:10:54 -0400

    adding into html files to induce the OAuth process

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -8,6 +8,18 @@ const createController = (app,db) => {
 		res.send("breh");
 	});
 
+	// end point to support Google OAuth flow
+	app.get("oauthcallback",(req,res) => {
+		console.log("req.query:",req.query);
+
+		// get req.query.code
+
+		//do some OAuth shit with code (probably store token)
+
+		// render index.html and start the app!
+		res.sendFile("main.html");
+	});
+
 	app.post("/emails",(req,res) => {
 		console.log("received request at /emails");
 		console.log("body is:",JSON.stringify(req.body));
