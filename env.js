feb409f2fbc17f8af4c57fb7c1c5d6f1bceaaaa4
davidmashe <davidmashe@gmail.com>
2017-08-01 07:57:35 -0400

    improved env.js again

--- env.js
+++ env.js
@@ -1,19 +1,23 @@
 // best pactice is to set environment variables
 // but as a backup this file can be used to set app-wide variables
 
-var USE_POSTGRES = false;
-var USE_OAUTH = false;
+var USE_POSTGRES;
+var USE_OAUTH;
 const MY_EMAIL = "your_email_goes_here@gmail.com"; // you must have a gmail address!
 const WEBPAGE_URL = "http://you.github.io";
 const RESUME_URL = "http://whereyouhostyourresume.com";
 
 if (process.env.USE_POSTGRES === "true") {
 	USE_POSTGRES = true;
-} 
+} else {
+	USE_POSTGRES = false; // modify here
+}
 
 if (process.env.USE_OAUTH === "true") {
 	USE_OAUTH = true;
-} 
+} else {
+	USE_OAUTH = false; // modify here
+}
 
 const env = {
 	USE_POSTGRES : USE_POSTGRES,
