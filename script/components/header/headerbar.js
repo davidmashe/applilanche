7a3b232166a6fcaf7d2e2a674ca2c129f41c4af6
Rachel Cohen <rfc@alumni.duke.edu>
2017-08-03 19:45:42 -0400

    closes #4

--- script/components/header/headerbar.js
+++ script/components/header/headerbar.js
@@ -1,38 +1,34 @@
 import React from 'react';
 
+import { Nav, NavItem } from 'react-bootstrap';
+
 class HeaderBar extends React.Component {
 
 	constructor(props){
 		super(props);
+		this.state = {
+			selected: '0'
+		}
+		this.handleSelect = this.handleSelect.bind(this);
+	}
 
+	handleSelect(eventKey){
+		event.preventDefault();
+		this.setState({ selected: eventKey });
+		eventKey === '2' ? alert("COMING SOON") : this.props.onClick(+eventKey);
 	}
 
 	render() {
 		return (
-			<div className="header-box">
-				<ul className="header-list">
-					<li 
-						className="header-item"
-						onClick={() => {this.props.onClick(0)}}
-					>
-						Enter Emails
-					</li>
-					<li 
-						className="header-item"
-						onClick={() => {this.props.onClick(1)}}
-					>
-						Apply Via Scraper
-					</li>	
-					<li
-						className="header-item"
-						onClick={() => {alert("COMING SOON")}}
-					>
-						Application History
-					</li>
-				</ul>
+		   <div>
+			  <Nav bsStyle="tabs" activeKey={this.state.selected} onSelect={this.handleSelect}>
+	        <NavItem eventKey="0">Enter Emails</NavItem>
+	        <NavItem eventKey="1">Apply Via Scraper</NavItem>
+	        <NavItem eventKey="2">Application History</NavItem>
+	      </Nav>
 			</div>
 		);
 	}
-} 
+}
 
 export default HeaderBar
